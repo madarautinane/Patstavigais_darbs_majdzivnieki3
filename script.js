baa365855d36890ed9dcc03ce0731296db112a25
madarautinane <madara.utinane@je.pvg.edu.lv>
2026-04-27 20:20:52 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,37 @@
 let score = 0;
+let time = 20;
 let gameInterval;
+let timerInterval;
 
 function startGame() {
     score = 0;
+    time = 20;
+
     document.getElementById("score").innerText = score;
+    document.getElementById("time").innerText = time;
+    document.getElementById("finalResult").innerText = "";
 
     clearInterval(gameInterval);
+    clearInterval(timerInterval);
 
-    gameInterval = setInterval(spawnAnimal, 1000);
+    gameInterval = setInterval(spawnAnimal, 800);
+
+    timerInterval = setInterval(() => {
+        time--;
+        document.getElementById("time").innerText = time;
+
+        if (time <= 0) {
+            endGame();
+        }
+    }, 1000);
 }
 
 function spawnAnimal() {
     const gameArea = document.getElementById("gameArea");
 
     const animal = document.createElement("div");
-
     animal.innerText = "🐶";
+
     animal.style.position = "absolute";
     animal.style.left = Math.random() * 250 + "px";
     animal.style.top = Math.random() * 200 + "px";
@@ -32,5 +48,13 @@ function spawnAnimal() {
 
     setTimeout(() => {
         animal.remove();
-    }, 1500);
+    }, 1200);
+}
+
+function endGame() {
+    clearInterval(gameInterval);
+    clearInterval(timerInterval);
+
+    document.getElementById("finalResult").innerText =
+        "🎉 Spēle beidzās! Tavi punkti: " + score;
 }
