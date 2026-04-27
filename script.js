ee2b761dabefdefe76da7c874ba0459aad80d03b
madarautinane <madara.utinane@je.pvg.edu.lv>
2026-04-27 19:38:15 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+function show(id) {
+    document.querySelectorAll("section").forEach(s => s.classList.remove("active"));
+    document.getElementById(id).classList.add("active");
+}
 
+function checkAnswer(answer) {
+    const result = document.getElementById("result");
+
+    if (answer === "suns") {
+        result.innerText = "✔ Pareizi! Tas ir suns 🐶";
+    } else {
+        result.innerText = "❌ Nepareizi, mēģini vēl!";
+    }
+}
